54af51a3b047c2e1537573f1db8ced8f98b4e34c
Ellison Leão <ellisonleao@gmail.com>
2020-12-23 02:52:36 -0300

    renamed go exercism folders, adding tree-building

---
+++ exercism/go/14-tree-building/tree_building.go
@@ -0,0 +1,69 @@
+package tree
+
+import (
+	"errors"
+	"sort"
+)
+
+type Record struct {
+	ID     int
+	Parent int
+}
+
+type Node struct {
+	ID       int
+	Children []*Node
+}
+
+type Records []Record
+
+var (
+	errRootWithParent = errors.New("Root must not have parents")
+	errNoRoot         = errors.New("tree does not have root node")
+	errMissingNode    = errors.New("missing node")
+)
+
+func (r Records) Len() int           { return len(r) }
+func (r Records) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
+func (r Records) Less(i, j int) bool { return r[i].ID < r[j].ID }
+
+func Build(records []Record) (*Node, error) {
+	if len(records) == 0 {
+		return nil, nil
+	}
+
+	sort.Sort(Records(records))
+
+	if records[0].ID != 0 {
+		return nil, errNoRoot
+	}
+
+	if records[0].Parent != 0 {
+		return nil, errRootWithParent
+	}
+
+	root := &Node{}
+	nodes := make([]*Node, len(records))
+	nodes[0] = root
+
+	for i := 1; i < len(records); i++ {
+		r := records[i]
+		if r.ID != i {
+			return nil, errMissingNode
+		}
+
+		child := &Node{ID: r.ID}
+		parent := nodes[r.Parent]
+		if parent == nil {
+			return nil, errMissingNode
+		}
+
+		if parent.Children == nil {
+			parent.Children = []*Node{}
+		}
+		parent.Children = append(parent.Children, child)
+		nodes[r.ID] = child
+	}
+
+	return root, nil
+}
