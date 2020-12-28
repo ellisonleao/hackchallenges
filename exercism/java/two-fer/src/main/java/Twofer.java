360bf9c9a56a552daf8b511e3198d6659b80b2e0
Ellison Leão <ellisonleao@gmail.com>
2020-12-28 13:38:15 -0300

    removing java and assembly on exercism

--- exercism/java/two-fer/src/main/java/Twofer.java
+++
@@ -1,8 +0,0 @@
-class Twofer {
-
-    String twofer(String name) {
-        String input = name;
-        input = input == null || input.isEmpty() ? "you" : input;
-        return String.format("One for %s, one for me.", input);
-    }
-}
